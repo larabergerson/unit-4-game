aca2b608368debbb8f44c0d7f0f5c2b408075481
lara bergerson <lara.bergerson@gmail.com>
2019-06-05 20:58:41 -0500

    added some script. docs aren't connecting...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,32 @@
-var auqa = 4
-var multi = 9
-var purple = 15
-var rainbow = 7
+var targetNumber = 61;
+    $(".match").text(targetNumber);
+
+
+var counter = 0
+var numberChoices = [4, 9, 15, 7];
+
+for (var i = 0; i < numberChoices.length; i++){
+
+    var aquaGem ()
+}
+
+$(".gems").on("click", function(){
+    alert("working");
+
+});
+
+
+// var gemList = {
+//     "aqua": 4
+//     "multi" : 9
+//     "purple": 15
+//     "rainbow": 7
+
+
+// }
+
+// var gems=[ 
+// var auqa = 4
+// var multi = 9
+// var purple = 15
+// var rainbow = 7
