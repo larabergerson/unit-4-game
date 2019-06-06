84468d4a20a21732243f424ab4b2c7eca188fca4
lara bergerson <lara.bergerson@gmail.com>
2019-06-05 22:21:34 -0500

    played around with JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,30 @@
 var targetNumber = 61;
     $(".match").text(targetNumber);
+    
     console.log(targetNumber)
-
-
 var counter = 0
+
 var numberChoices = [4, 9, 15, 7];
+var increments = numberChoices[Math.round(Math.random())];
+$(".gems-images").on("click", function(){
+counter += increments;
+    alert("Your score:" + counter);
+
 
 for (var i = 0; i < numberChoices.length; i++){
-
+// var crystals = {
+//     blue : 4,
+//     multi : 9,
+//     purple : 15,
+//     rainbow : 7
+//  };
 
 }
 
-$(".gems-images").on("click", function(){
-    alert("working");
 
-});
+
+
+    
 
 
 // var gemList = {
